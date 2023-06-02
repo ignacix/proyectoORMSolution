d7fb07810352e50ed359d050a5abc0b694329165
ignacix <ignanj8mail.com>
2023-06-02 17:18:24 -0300

    Agregar archivos de proyecto.

---
+++ proyectoORM/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace proyectoORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btninsertar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCantCanciones = new System.Windows.Forms.TextBox();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnOpcionInsertar = new System.Windows.Forms.Button();
+            this.btnOpcionModificar = new System.Windows.Forms.Button();
+            this.btnOpcionEliminar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombreModificar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtIdEliminar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(748, 155);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btninsertar
+            // 
+            this.btninsertar.Location = new System.Drawing.Point(278, 53);
+            this.btninsertar.Name = "btninsertar";
+            this.btninsertar.Size = new System.Drawing.Size(75, 23);
+            this.btninsertar.TabIndex = 1;
+            this.btninsertar.Text = "insertar";
+            this.btninsertar.UseVisualStyleBackColor = true;
+            this.btninsertar.Click += new System.EventHandler(this.btninsertar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(9, 56);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtCantCanciones
+            // 
+            this.txtCantCanciones.Location = new System.Drawing.Point(146, 56);
+            this.txtCantCanciones.Name = "txtCantCanciones";
+            this.txtCantCanciones.Size = new System.Drawing.Size(100, 20);
+            this.txtCantCanciones.TabIndex = 3;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(288, 49);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 4;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(10, 49);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCantCanciones);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.btninsertar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 206);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 106);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insertar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cant. Canciones:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.btnModificar);
+            this.groupBox2.Controls.Add(this.txtNombreModificar);
+            this.groupBox2.Location = new System.Drawing.Point(21, 318);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modificar";
+            // 
+            // btnOpcionInsertar
+            // 
+            this.btnOpcionInsertar.BackColor = System.Drawing.Color.Transparent;
+            this.btnOpcionInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpcionInsertar.Location = new System.Drawing.Point(15, 0);
+            this.btnOpcionInsertar.Name = "btnOpcionInsertar";
+            this.btnOpcionInsertar.Size = new System.Drawing.Size(124, 44);
+            this.btnOpcionInsertar.TabIndex = 8;
+            this.btnOpcionInsertar.Text = "Insertar";
+            this.btnOpcionInsertar.UseVisualStyleBackColor = false;
+            this.btnOpcionInsertar.Click += new System.EventHandler(this.btnSeleccion_Click);
+            // 
+            // btnOpcionModificar
+            // 
+            this.btnOpcionModificar.BackColor = System.Drawing.Color.Transparent;
+            this.btnOpcionModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpcionModificar.Location = new System.Drawing.Point(147, 0);
+            this.btnOpcionModificar.Name = "btnOpcionModificar";
+            this.btnOpcionModificar.Size = new System.Drawing.Size(124, 44);
+            this.btnOpcionModificar.TabIndex = 9;
+            this.btnOpcionModificar.Text = "Modificar";
+            this.btnOpcionModificar.UseVisualStyleBackColor = false;
+            this.btnOpcionModificar.Click += new System.EventHandler(this.btnOpcionModificar_Click);
+            // 
+            // btnOpcionEliminar
+            // 
+            this.btnOpcionEliminar.BackColor = System.Drawing.Color.Transparent;
+            this.btnOpcionEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpcionEliminar.Location = new System.Drawing.Point(279, 0);
+            this.btnOpcionEliminar.Name = "btnOpcionEliminar";
+            this.btnOpcionEliminar.Size = new System.Drawing.Size(124, 44);
+            this.btnOpcionEliminar.TabIndex = 10;
+            this.btnOpcionEliminar.Text = "Eliminar";
+            this.btnOpcionEliminar.UseVisualStyleBackColor = false;
+            this.btnOpcionEliminar.Click += new System.EventHandler(this.btnOpcionEliminar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.btnOpcionEliminar);
+            this.panel1.Controls.Add(this.btnOpcionModificar);
+            this.panel1.Controls.Add(this.btnOpcionInsertar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 44);
+            this.panel1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Id:";
+            // 
+            // txtNombreModificar
+            // 
+            this.txtNombreModificar.Location = new System.Drawing.Point(137, 49);
+            this.txtNombreModificar.Name = "txtNombreModificar";
+            this.txtNombreModificar.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreModificar.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nombre:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnEliminar);
+            this.groupBox3.Controls.Add(this.txtIdEliminar);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(470, 274);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(242, 100);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Eliminar";
+            // 
+            // txtIdEliminar
+            // 
+            this.txtIdEliminar.Location = new System.Drawing.Point(25, 50);
+            this.txtIdEliminar.Name = "txtIdEliminar";
+            this.txtIdEliminar.Size = new System.Drawing.Size(100, 20);
+            this.txtIdEliminar.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Id:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(149, 50);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "CRUD DISCOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btninsertar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCantCanciones;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOpcionInsertar;
+        private System.Windows.Forms.Button btnOpcionModificar;
+        private System.Windows.Forms.Button btnOpcionEliminar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombreModificar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtIdEliminar;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ proyectoORM/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refrescarGrilla();
+
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+        }
+
+        private void btninsertar_Click(object sender, EventArgs e)
+        {
+            DISCOS disco = new DISCOS();
+
+            disco.Titulo = txtNombre.Text;
+            disco.CantidadCanciones = int.Parse(txtCantCanciones.Text);
+
+            using (DISCOS_DBEntities db = new DISCOS_DBEntities())
+            {
+                db.DISCOS.Add(disco);
+                db.SaveChanges();
+                refrescarGrilla();
+            }
+
+
+        }
+
+
+        private void refrescarGrilla()
+        {
+            using (DISCOS_DBEntities db = new DISCOS_DBEntities())
+            {
+                var tabla = db.DISCOS;
+                List<DISCOS> lista = new List<DISCOS>();
+
+                foreach (DISCOS discos in tabla)
+                {
+                    lista.Add(discos);
+                }
+
+                dataGridView1.DataSource = lista;
+
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            using (DISCOS_DBEntities db = new DISCOS_DBEntities())
+            {
+                DISCOS disco = db.DISCOS.Find(int.Parse(txtId.Text));                
+                disco.Titulo = txtNombreModificar.Text;
+
+                db.Entry(disco).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                refrescarGrilla();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            DISCOS discActual = dataGridView1.CurrentRow.DataBoundItem as DISCOS;
+            
+            if (groupBox2.Visible==true)
+            {
+                txtId.Text = discActual.Id.ToString();
+                txtNombreModificar.Text = discActual.Titulo.ToString();
+            }
+            else if (groupBox3.Visible == true)
+            {
+                txtIdEliminar.Text = discActual.Id.ToString();                
+            }
+
+
+        }
+
+        private void btnSeleccion_Click(object sender, EventArgs e)
+        {
+            HacerVsible(groupBox1);                        
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+
+        }
+
+        private void HacerVsible(GroupBox gbx)
+        {
+            gbx.Visible = true;
+            Point point = new Point(12, 206);
+            gbx.Location = point;
+        }
+
+        private void btnOpcionModificar_Click(object sender, EventArgs e)
+        {
+            HacerVsible(groupBox2);
+            groupBox1.Visible = false;
+            groupBox3.Visible=false;
+        }
+
+        private void btnOpcionEliminar_Click(object sender, EventArgs e)
+        {
+            HacerVsible(groupBox3);
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            using (DISCOS_DBEntities db = new DISCOS_DBEntities())
+            {
+                DISCOS disco = db.DISCOS.Find(int.Parse(txtIdEliminar.Text));
+
+                DialogResult resultado = MessageBox.Show("Seguro que quiere Eliminarlo", "camption", MessageBoxButtons.YesNo);
+                if(resultado==DialogResult.Yes)
+                {
+                    db.DISCOS.Remove(disco);
+                    db.Entry(disco).State = System.Data.Entity.EntityState.Deleted;
+                    db.SaveChanges();
+                    refrescarGrilla();
+
+                }
+            }
+        }
+    }
+}
